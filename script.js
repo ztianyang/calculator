161567e0c4f959d063b9ccfcd41552f6c47a995c
Tianyang Zhang <tianyangzhang515@gmail.com>
2023-11-09 09:34:49 -0800

    Add functionality for operator buttons

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const maxLength = 14;
 
 const output = document.querySelector("#output");
 const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
 
 function add(num1, num2) {
     return num1 + num2;
@@ -39,20 +40,46 @@ function operate(num1, num2, op) {
     } else if (op === "/") {
         return divide(num1, num2);
     } else {
+        error = true;
         return "UNKNOWN OPERATOR";
     }
 }
 
 digitButtons.forEach((digitButton) => {
     digitButton.addEventListener("click", (e) => {
-        let digit = e.target.textContent;
-        if (display === "0") {
-            display = digit;
-        } else {
-            if (display.length < maxLength) {
-                display = display + digit;
+        if (!error) {
+            let digit = e.target.textContent;
+            if (op !== "" && num1 === null) {
+                num1 = Number.parseFloat(display);
+                display = "0";
             }
+            if (display === "0") {
+                display = digit;
+            } else {
+                if (display.length < maxLength) {
+                    display = display + digit;
+                }
+            }
+            output.textContent = display;
         }
-        output.textContent = display;
+    });
+});
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", (e) => {
+        if (!error) {
+            let operator = e.target.textContent;
+            if (num1 === null) {
+                op = operator;
+            } else {
+                num2 = Number.parseFloat(display);
+                let result = operate(num1, num2, op);
+                display = `${result}`;
+                num1 = null;
+                num2 = null;
+                op = operator;
+            }
+            output.textContent = display;
+        }
     });
 });
